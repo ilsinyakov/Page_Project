e6d3f1b6e1146ae2a948c65d6db0f358a5cbb478
Синяков Илья Леонидович <ilsinyakov@gmail.com>
2024-01-18 14:14:45 +0300

    Final Prepeare 01

--- test_product_page.py
+++ test_product_page.py
@@ -51,15 +51,15 @@ class TestUserAddToBasketFromProductPage():
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 '''
 
-@pytest.mark.skip(reason="no way of currently testing this") # пропускаем тест
+#@pytest.mark.skip(reason="no way of currently testing this") # пропускаем тест
 def test_guest_can_add_product_to_basket(browser): # <- вставить link в атрибуты при включении параметризации!
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207' # ссылка без промо
-#    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear' # ссылка с промо (!включить квиз)
+#    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207' # ссылка без промо
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear' # ссылка с промо (!включить квиз)
     page = ProductPage(browser, link) # создаем объект класса ProductPage
     page.open() # открываем страницу по ссылке
     page.should_be_add_to_basket_button() # проверяем наличие кнопки "Добавить в корзину"
     page.add_product_to_basket() # проверяем добавление товара в корзину
-#    page.solve_quiz_and_get_code() # решаем квиз
+    page.solve_quiz_and_get_code() # решаем квиз
     page.should_be_product_in_basket() # проверяем, та ли книга у нас в корзине
     page.should_be_basket_cost_same_product_price() # проверяем, совпадает ли стоимость корзины с ценой книги
 
